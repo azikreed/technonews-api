24327ee228fb2749e7bb9f584a693756209fc542
azikreed <azikreed@gmail.com>
2024-03-07 19:49:21 +0300

    [+] initialization finished 90%

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { ILogger } from './interfaces/logger.interface';
+import { TYPES } from './types';
+import { LoggerService } from './helpers/logger/logger.service';
+import { App } from './app';
+import 'reflect-metadata';
+import { ConfigService } from './helpers/config/config.service';
+import { IConfigService } from './interfaces/config.interface';
+
+const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.LoggerService).to(LoggerService).inSingletonScope();
+	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
+	bind<App>(TYPES.Application).to(App);
+});
+
+interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
+}
+
+async function bootstrap(): Promise<IBootstrapReturn> {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	await app.init();
+	return { app, appContainer };
+}
+
+export const boot = bootstrap();
