246054f0bfc22c4964463b16d65ef1b2192ddb51
azikreed <azikreed@gmail.com>
2024-03-07 19:49:38 +0300

    [+] all types added

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export const TYPES = {
+	Application: Symbol.for('Application'),
+	LoggerService: Symbol.for('LoggerService'),
+	ConfigService: Symbol.for('ConfigService'),
+	ExceptionFilter: Symbol.for('ExceptionFilter'),
+};
