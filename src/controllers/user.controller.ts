e8c7125835d11b31f8420efa1d940e41b38d4998
azikreed <azikreed@gmail.com>
2024-03-08 19:27:43 +0300

    [+] basic user controller class

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+import { inject } from 'inversify';
+import { BaseController } from './base.controller';
+import { TYPES } from '../types';
+import { ILogger } from '../interfaces/logger.interface';
+import { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify';
+import { IUserController } from '../interfaces/user.controller.interface';
+
+export class UserController extends BaseController implements IUserController {
+	constructor(@inject(TYPES.LoggerService) private loggerService: ILogger) {
+		super(loggerService);
+
+		this.bindRoutes([
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register.bind(this),
+				middlewares: [],
+			},
+		]);
+	}
+
+	async register(request: FastifyRequest, reply: FastifyReply): Promise<void> {}
+	async login(request: FastifyRequest, reply: FastifyReply): Promise<void> {}
+	async refresh(request: FastifyRequest, reply: FastifyReply): Promise<void> {}
+}
