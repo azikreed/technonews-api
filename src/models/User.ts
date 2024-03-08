4e72eac5fe33e4c166ee9c364628df95fe70a980
azikreed <azikreed@gmail.com>
2024-03-08 22:03:06 +0300

    [+] user model

---
+++ src/models/User.ts
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUserModel } from '../interfaces/user.model.interface';
+
+const userSchema = new Schema<IUserModel>({
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	name: { type: String, required: true },
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now },
+});
+
+export const UserModel = mongoose.model<IUserModel>('User', userSchema);
