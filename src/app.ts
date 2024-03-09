c8500a6fcba1a0109cbc6c59bf824f883b1a442b
azikreed <azikreed@gmail.com>
2024-03-09 18:08:23 +0300

    [*] moved to express

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,57 @@
-import Fastify, { FastifyInstance } from 'fastify';
+import express, { Express } from 'express';
 import { Server } from 'http';
 import { inject, injectable } from 'inversify';
 import { TYPES } from './types';
-import { IConfigService } from './interfaces/config.interface';
 import { ILogger } from './interfaces/logger.interface';
+import { json } from 'body-parser';
+import { IExceptionFilter } from './interfaces/exception.filter.interface';
 import 'reflect-metadata';
-
+import { IConfigService } from './interfaces/config.interface';
+import { MongoService } from './services/db.service';
+import { UserController } from './controllers/user.controller';
+import { AuthMiddleware } from './middlewares/auth.middleware';
+import cors from 'cors';
 @injectable()
 export class App {
-	app: FastifyInstance;
-	port: number;
+	app: Express;
 	server: Server;
+	port: number;
 
 	constructor(
-		@inject(TYPES.LoggerService) private loggerService: ILogger,
+		@inject(TYPES.LoggerService) private logger: ILogger,
+		@inject(TYPES.ExceptionFilter) private exceptionFilter: IExceptionFilter,
 		@inject(TYPES.ConfigService) private configService: IConfigService,
+		@inject(TYPES.MongoService) private mongoService: MongoService,
+		@inject(TYPES.UserController) private userController: UserController,
 	) {
-		this.app = Fastify();
+		this.app = express();
 		this.port = Number(this.configService.get('PORT'));
 	}
 
-	useMiddleware(): void {}
+	useMiddleware(): void {
+		this.app.use(json());
+		this.app.use(cors());
+		const authMiddleware = new AuthMiddleware(this.configService.get('SECRET'));
+		this.app.use(authMiddleware.execute.bind(authMiddleware));
+	}
 
-	useRoutes(): void {}
+	useRoutes(): void {
+		this.app.use('/user', this.userController.router);
+	}
+
+	useExceptionFilters(): void {}
 
 	public async init(): Promise<void> {
 		this.useMiddleware();
 		this.useRoutes();
-		this.server = this.app.server.listen(this.port);
-		this.loggerService.log(`Server started on port ${this.port}`);
+		this.useExceptionFilters();
+		await this.mongoService.connect();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Server has been started on http://localhost:${this.port}`);
 	}
 
 	public close(): void {
+		this.mongoService.disconnect();
 		this.server.close();
 	}
 }
