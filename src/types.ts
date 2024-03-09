535ff011873baa6fe5483af8e1bacd1a0228a647
azikreed <azikreed@gmail.com>
2024-03-09 18:10:36 +0300

    [+] mongoservice, userservice, userrepo

--- src/types.ts
+++ src/types.ts
@@ -3,5 +3,8 @@ export const TYPES = {
 	LoggerService: Symbol.for('LoggerService'),
 	ConfigService: Symbol.for('ConfigService'),
 	ExceptionFilter: Symbol.for('ExceptionFilter'),
+	MongoService: Symbol.for('MongoService'),
 	UserController: Symbol.for('UserController'),
+	UserService: Symbol.for('UserService'),
+	UserRepository: Symbol.for('UserRepository'),
 };
