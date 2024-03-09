dd8066cc704b2cd9e0756cc7d03b52f02f5498a6
azikreed <azikreed@gmail.com>
2024-03-09 18:08:29 +0300

    [*] moved to express

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,27 @@ import { Container, ContainerModule, interfaces } from 'inversify';
 import { ILogger } from './interfaces/logger.interface';
 import { TYPES } from './types';
 import { LoggerService } from './helpers/logger/logger.service';
+import { IExceptionFilter } from './interfaces/exception.filter.interface';
+import { ExceptionFilter } from './helpers/errors/exception.filter';
 import { App } from './app';
-import 'reflect-metadata';
-import { ConfigService } from './helpers/config/config.service';
 import { IConfigService } from './interfaces/config.interface';
+import { ConfigService } from './helpers/config/config.service';
+import { MongoService } from './services/db.service';
 import { UserController } from './controllers/user.controller';
 import { IUserController } from './interfaces/user.controller.interface';
+import { UserService } from './services/user.service';
+import { IUserService } from './interfaces/user.service.interface';
+import { UserRepository } from './repositories/user.repository';
+import { IUserRepository } from './interfaces/user.repository.interface';
 
 const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.LoggerService).to(LoggerService).inSingletonScope();
+	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+	// bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserRepository>(TYPES.UserRepository).to(UserRepository).inSingletonScope();
 	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
-	bind<IUserController>(TYPES.UserController).to(UserController).inSingletonScope();
+	bind<IUserService>(TYPES.UserService).to(UserService).inSingletonScope();
+	bind<MongoService>(TYPES.MongoService).to(MongoService).inSingletonScope();
 	bind<App>(TYPES.Application).to(App);
 });
 
