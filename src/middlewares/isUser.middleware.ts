473ebdcef1e38d462dfadfe2ef811f3b345839c1
azikreed <azikreed@gmail.com>
2024-03-10 08:23:19 +0300

    [+] isUser middleware

---
+++ src/middlewares/isUser.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+import { IMiddleware } from '../interfaces/middleware.interface';
+
+export class IsUserMiddleware implements IMiddleware {
+	execute(req: Request, res: Response, next: NextFunction): void {
+		if (req.user.role === 'user') {
+			return next();
+		}
+		res.status(401).send({ err: 'Вы не авторизованы' });
+	}
+}
