9934974b7200317164141960cb3ffd42ae44b355
azikreed <azikreed@gmail.com>
2024-03-10 18:01:46 +0300

    [+] upload repository

---
+++ src/repositories/upload.repository.ts
@@ -0,0 +1,15 @@
+import { injectable } from 'inversify';
+import { Upload } from '../entities/upload.entity';
+import { IUploadModel } from '../interfaces/upload.model.interface';
+import { IUploadRepository } from '../interfaces/upload.repository.interface';
+import { UploadModel } from '../models/Upload';
+
+@injectable()
+export class UploadRepository implements IUploadRepository {
+	constructor() {}
+
+	async create({ data }: Upload): Promise<IUploadModel> {
+		const upload = new UploadModel({ data });
+		return await upload.save();
+	}
+}
