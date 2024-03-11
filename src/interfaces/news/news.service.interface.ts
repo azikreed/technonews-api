359b3d722e19f98f38e961592f86e481390324bf
azikreed <azikreed@gmail.com>
2024-03-11 16:20:14 +0300

    [+] news service interface

---
+++ src/interfaces/news/news.service.interface.ts
@@ -0,0 +1,9 @@
+import { News } from '../../entities/news.entity';
+import { INewsModel } from './news.model.interface';
+import { INewsUpdate } from './news.update.interface';
+
+export interface INewsService {
+	create: (news: News) => Promise<INewsModel | null>;
+	find: (id: string) => Promise<INewsModel | null>;
+	update: (id: string, data: INewsUpdate) => Promise<INewsModel | null>;
+}
