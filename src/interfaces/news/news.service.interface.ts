1a98303c51d609c425251ae07f0377e24e2db9c9
azikreed <azikreed@gmail.com>
2024-03-11 20:34:29 +0300

    [+] findAll method service interface

--- src/interfaces/news/news.service.interface.ts
+++ src/interfaces/news/news.service.interface.ts
@@ -5,5 +5,6 @@ import { INewsUpdate } from './news.update.interface';
 export interface INewsService {
 	create: (news: News) => Promise<INewsModel | null>;
 	find: (id: string) => Promise<INewsModel | null>;
+	findAll: () => Promise<INewsModel[] | null>;
 	update: (id: string, data: INewsUpdate) => Promise<INewsModel | null>;
 }
