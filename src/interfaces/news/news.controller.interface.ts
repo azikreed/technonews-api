3b8dfa8c677e382f3f927627fea19cfca7fe1a28
azikreed <azikreed@gmail.com>
2024-03-11 16:19:48 +0300

    [+] news controller interface

---
+++ src/interfaces/news/news.controller.interface.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from 'express';
+
+export interface INewsController {
+	createNews: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+	viewIncrement: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+}
