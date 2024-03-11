1ffc8b5868e72655154405d9402c708e9170746d
azikreed <azikreed@gmail.com>
2024-03-11 16:21:23 +0300

    [+] news repository

---
+++ src/repositories/news.repository.ts
@@ -0,0 +1,24 @@
+import { injectable } from 'inversify';
+import { News } from '../entities/news.entity';
+import { INewsModel } from '../interfaces/news/news.model.interface';
+import { INewsRepository } from '../interfaces/news/news.repository.interface';
+import { NewsModel } from '../models/News';
+import { INewsUpdate } from '../interfaces/news/news.update.interface';
+
+@injectable()
+export class NewsRepository implements INewsRepository {
+	constructor() {}
+
+	async create(news: News): Promise<INewsModel | null> {
+		const createdNews = new NewsModel(news);
+		return createdNews.save();
+	}
+
+	async find(id: string): Promise<INewsModel | null> {
+		return await NewsModel.findOne({ _id: id });
+	}
+
+	async update(id: string, data: INewsUpdate): Promise<INewsModel | null> {
+		return await NewsModel.findByIdAndUpdate(id, data);
+	}
+}
