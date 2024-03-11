a498d449f404680892c93ece9b4f6dbc04b6b0c9
azikreed <azikreed@gmail.com>
2024-03-11 11:26:39 +0300

    [+] upload controller interface

--- src/interfaces/upload/upload.controller.interface.ts
+++ src/interfaces/upload/upload.controller.interface.ts
@@ -3,4 +3,5 @@ import { NextFunction, Request, Response } from 'express';
 export interface IUploadController {
 	createUpload: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 	deleteUpload: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+	getUploads: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 }
