b4cb48930244aa4607cce4fe8398801431a33bbf
azikreed <azikreed@gmail.com>
2024-03-11 11:26:46 +0300

    [+] upload repository interface

--- src/interfaces/upload/upload.repository.interface.ts
+++ src/interfaces/upload/upload.repository.interface.ts
@@ -4,5 +4,6 @@ import { IUploadModel } from './upload.model.interface';
 export interface IUploadRepository {
 	create: (upload: Upload) => Promise<IUploadModel | null>;
 	find: (id: string) => Promise<IUploadModel | null>;
+	findAll: () => Promise<IUploadModel[] | null>;
 	delete: (id: string) => Promise<boolean>;
 }
