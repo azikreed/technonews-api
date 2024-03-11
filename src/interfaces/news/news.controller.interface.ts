33274d61046bf83652b61057aaaf40d798de71c5
azikreed <azikreed@gmail.com>
2024-03-11 20:33:56 +0300

    [+] get all method interface

--- src/interfaces/news/news.controller.interface.ts
+++ src/interfaces/news/news.controller.interface.ts
@@ -3,4 +3,5 @@ import { NextFunction, Request, Response } from 'express';
 export interface INewsController {
 	createNews: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 	viewIncrement: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+	getAll: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 }
