e61f69a702fd025a854304a5fbfc0849e90b0f77
azikreed <azikreed@gmail.com>
2024-03-12 18:35:08 +0300

    [+] delete method interface

--- src/interfaces/news/news.controller.interface.ts
+++ src/interfaces/news/news.controller.interface.ts
@@ -4,4 +4,5 @@ export interface INewsController {
 	createNews: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 	viewIncrement: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 	getAll: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+	delete: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 }
