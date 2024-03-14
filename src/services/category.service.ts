58cc9b54e7fd6320f451a2eea1022f073690d0a9
azikreed <azikreed@gmail.com>
2024-03-14 13:42:16 +0300

    [+] category service

---
+++ src/services/category.service.ts
@@ -0,0 +1,15 @@
+import { inject, injectable } from 'inversify';
+import { Category } from '../entities/category.entity';
+import { ICategoryModel } from '../interfaces/category/category.model.interface';
+import { ICategoryService } from '../interfaces/category/category.service.interface';
+import { TYPES } from '../types';
+import { ICategoryRepository } from '../interfaces/category/category.repository.interface';
+
+@injectable()
+export class CategoryService implements ICategoryService {
+	constructor(@inject(TYPES.CategoryRepository) private categoryRepository: ICategoryRepository) {}
+
+	async create(category: Category): Promise<ICategoryModel | null> {
+		return await this.categoryRepository.create(category);
+	}
+}
