17b6d7b8ca2f1c53488b4471b3ff96ebad6970a2
azikreed <azikreed@gmail.com>
2024-03-14 09:54:42 +0300

    [+] pagination logic to getAll method of service interface

--- src/interfaces/news/news.service.interface.ts
+++ src/interfaces/news/news.service.interface.ts
@@ -1,11 +1,12 @@
 import { News } from '../../entities/news.entity';
+import { IPagination } from '../others/pagination.interface';
 import { INewsModel } from './news.model.interface';
 import { INewsUpdate } from './news.update.interface';
 
 export interface INewsService {
 	create: (news: News) => Promise<INewsModel | null>;
 	find: (id: string) => Promise<INewsModel | null>;
-	findAll: () => Promise<INewsModel[] | null>;
+	findAll: (pagination: IPagination) => Promise<INewsModel[] | null>;
 	update: (id: string, data: INewsUpdate) => Promise<INewsModel | null>;
 	delete: (id: string) => Promise<boolean | null>;
 	mostViewed: (limit: number) => Promise<INewsModel[] | null>;
