a5a39e914721b68b97608ebc72fa4802babc5f19
azikreed <azikreed@gmail.com>
2024-03-14 21:18:40 +0300

    [+] interface of delete method

--- src/interfaces/category/category.controller.interface.ts
+++ src/interfaces/category/category.controller.interface.ts
@@ -2,4 +2,5 @@ import { NextFunction, Request, Response } from 'express';
 
 export interface ICategoryController {
 	createCategory: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+	deleteCategory: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 }
