0d04663865de814b9cbd8fd310b915e54f38617e
azikreed <azikreed@gmail.com>
2024-03-14 21:18:59 +0300

    [+] interface of delete service

--- src/interfaces/category/category.service.interface.ts
+++ src/interfaces/category/category.service.interface.ts
@@ -3,4 +3,5 @@ import { ICategoryModel } from './category.model.interface';
 
 export interface ICategoryService {
 	create: (category: Category) => Promise<ICategoryModel | null>;
+	delete: (id: string) => Promise<boolean | null>;
 }
