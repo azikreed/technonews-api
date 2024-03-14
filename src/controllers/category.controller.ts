c259d9c2f3af5e5cae8606db011550bc52d79739
azikreed <azikreed@gmail.com>
2024-03-14 21:18:29 +0300

    [+] delete method

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -7,6 +7,7 @@ import { BaseController } from './base.controller';
 import { NextFunction, Request, Response } from 'express';
 import { ICategoryService } from '../interfaces/category/category.service.interface';
 import { HTTPError } from '../helpers/errors/http-error.class';
+import { INewsService } from '../interfaces/news/news.service.interface';
 
 @injectable()
 export class CategoryController extends BaseController implements ICategoryController {
@@ -14,6 +15,7 @@ export class CategoryController extends BaseController implements ICategoryContr
 		@inject(TYPES.LoggerService) private loggerService: ILogger,
 		@inject(TYPES.ConfigService) private configService: IConfigService,
 		@inject(TYPES.CategoryService) private categoryService: ICategoryService,
+		@inject(TYPES.NewsService) private newsService: INewsService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -21,6 +23,12 @@ export class CategoryController extends BaseController implements ICategoryContr
 				path: '/create',
 				method: 'post',
 				func: this.createCategory,
+				middlewares: [],
+			},
+			{
+				path: '/delete/:id',
+				method: 'delete',
+				func: this.deleteCategory,
 				middlewares: [],
 			},
 		]);
@@ -31,6 +39,24 @@ export class CategoryController extends BaseController implements ICategoryContr
 			const result = await this.categoryService.create(req.body);
 			if (!result) {
 				return next(new HTTPError(422, 'Ошибка при создании категорию', 'create category'));
+			}
+			this.ok(res, result);
+		} catch (e) {
+			this.send(res, 422, 'Ошибка при создании');
+		}
+	}
+
+	async deleteCategory(req: Request, res: Response, next: NextFunction): Promise<void> {
+		try {
+			const { id } = req.params;
+			const checkExist = await this.newsService.findByCategory(id);
+			console.log(checkExist);
+			if (checkExist?.length) {
+				return next(new HTTPError(422, 'Нельзя удалить категории с новостями', 'delete category'));
+			}
+			const result = await this.categoryService.delete(id);
+			if (!result) {
+				return next(new HTTPError(422, 'Ошибка при удалении категорию', 'delete category'));
 			}
 			this.ok(res, result);
 		} catch (e) {
