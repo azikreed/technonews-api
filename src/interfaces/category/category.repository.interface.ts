692bb0d8729a65bf506a2e9b7529075742cc5400
azikreed <azikreed@gmail.com>
2024-03-15 20:22:55 +0300

    [+] category repository - find all method interface

--- src/interfaces/category/category.repository.interface.ts
+++ src/interfaces/category/category.repository.interface.ts
@@ -1,7 +1,9 @@
+import { FilterQuery } from 'mongoose';
 import { Category } from '../../entities/category.entity';
 import { ICategoryModel } from './category.model.interface';
 
 export interface ICategoryRepository {
 	create: (category: Category) => Promise<ICategoryModel | null>;
+	find: () => Promise<ICategoryModel[] | null>;
 	delete: (id: string) => Promise<boolean | null>;
 }
