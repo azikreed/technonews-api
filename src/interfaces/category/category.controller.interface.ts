f18b86efdb1d468a66f3a76406025f0b7dcbe5e3
azikreed <azikreed@gmail.com>
2024-03-15 20:22:47 +0300

    [+] category controller - find all method interface

--- src/interfaces/category/category.controller.interface.ts
+++ src/interfaces/category/category.controller.interface.ts
@@ -2,5 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 
 export interface ICategoryController {
 	createCategory: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+	getAll: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 	deleteCategory: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 }
