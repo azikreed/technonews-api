40abbfd50766df51b4fe2e360f2d221d37c3cb28
azikreed <azikreed@gmail.com>
2024-03-16 21:30:16 +0300

    [+] repository interface of get one method

--- src/interfaces/category/category.repository.interface.ts
+++ src/interfaces/category/category.repository.interface.ts
@@ -6,4 +6,5 @@ export interface ICategoryRepository {
 	create: (category: Category) => Promise<ICategoryModel | null>;
 	find: () => Promise<ICategoryModel[] | null>;
 	delete: (id: string) => Promise<boolean | null>;
+	findOne: (id: string) => Promise<ICategoryModel | null>;
 }
