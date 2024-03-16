e0497f5fbd5d55ef14cdfd94b67a4e669f441b95
azikreed <azikreed@gmail.com>
2024-03-16 21:29:55 +0300

    [+] get one method interface

--- src/interfaces/category/category.controller.interface.ts
+++ src/interfaces/category/category.controller.interface.ts
@@ -4,4 +4,5 @@ export interface ICategoryController {
 	createCategory: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 	getAll: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 	deleteCategory: (req: Request, res: Response, next: NextFunction) => Promise<void>;
+	getOne: (req: Request, res: Response, next: NextFunction) => Promise<void>;
 }
